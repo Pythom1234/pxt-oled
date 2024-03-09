ecb5166071e3284925e17c88a3beb59a8e003b1c
Pythom1234 <pythom1234@email.cz>
2024-03-09 09:08:51 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -92,7 +92,7 @@ namespace OLED {
     }
     //% block="add text $txt at|x $x|y $y|color $color"
     //% color.defl=true
-    //% txt.defl=""
+    //% txt.defl="aaa"
     //% weight=95
     export function text(txt: any, x: number, y: number, color: boolean): void {
         const font = [
