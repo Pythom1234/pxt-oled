b4699fd20fd1236e587a74a6b09b925a9dc3a90c
Pythom1234 <pythom1234@email.cz>
2024-06-08 17:45:20 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@
 //% icon="\uf26c" color=#0000ff
 namespace OLED {
     const ADDR = 0x3C
+    const DEFAULT_IMAGE = imageio.createImage5x5(`
+. . . . .
+. . . . .
+. . # . .
+. . . . .
+. . . . .
+`)
     let screen = pins.createBuffer(1025)
 
     function cmd1(cmd1: number): void {
@@ -303,7 +310,7 @@ namespace OLED {
     //% color.defl=true
     //% bg.defl=false
     //% toggle.defl=false
-    //% image.defl=Image
+    //% image.defl=DEFAULT_IMAGE
     //% weight=91
     export function drawImage(image: Image, x: number, y: number, color: boolean, bg: boolean, toggle: boolean): void {
         for (let img_x = 0; img_x < image.width(); img_x++) {
