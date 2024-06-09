e6eece45f0abaad08dfe69c85360b271b8258463
Pythom1234 <pythom1234@email.cz>
2024-06-09 19:10:59 +0200

    Aktualizovat test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,23 @@
 OLED.init()
 OLED.clear(false)
 for (let i = 0; i < 20; i++) {
-    OLED.setPx(i, i, true)
-    OLED.draw()
+    OLED.setPx(i, i / 2, true)
 }
+OLED.draw()
 OLED.drawText("some text", 0, 20, true, false)
 OLED.draw()
-OLED.drawRect(0, 0, 32, 16, true, true, true)
+OLED.drawRect(0, 0, 127, 64, true, true, true)
 OLED.draw()
-basic.pause(100)
-OLED.drawLine(20, 0, 30, 30, false, false)
+basic.pause(500)
+OLED.drawLine(0, 20, 72, 28, false, false)
+OLED.drawLine(0, 28, 72, 20, false, false)
+OLED.draw()
+basic.pause(500)
+OLED.drawImage(imageio.createImage5x5(`
+. . . . .
+. # . # .
+. . # . .
+. # . # .
+. . . . .
+`), 50, 50, true, true, false)
 OLED.draw()
