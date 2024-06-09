5dcedcb5ef2ef3aa864a6b2b54a3bad2cc1513a4
Pythom1234 <pythom1234@email.cz>
2024-06-09 16:48:51 +0200

    Aktualizovat main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,13 @@
 OLED.init()
-basic.forever(function() {
-    OLED.clear(false)
-    for (let i = 0; i < 20; i++) {
-        OLED.setPx(i, i, true)
-        OLED.draw()
-    }
-    OLED.drawText("some text", 0, 20, true, false)
+OLED.clear(false)
+for (let i = 0; i < 20; i++) {
+    OLED.setPx(i, i, true)
     OLED.draw()
-    OLED.drawRect(0, 0, 127, 63, true, true, true)
-    OLED.draw()
-    OLED.drawLine(20, 0, 30, 30, false, false)
-    OLED.draw()
-})
+}
+OLED.drawText("some text", 0, 20, true, false)
+OLED.draw()
+OLED.drawRect(0, 0, 32, 16, true, true, true)
+OLED.draw()
+basic.pause(100)
+OLED.drawLine(20, 0, 30, 30, false, false)
+OLED.draw()
